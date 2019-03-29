97e5eac08b7cc9a42daf078e8a8643c9f8b78650
JamieDF <jamiedf8@gmail.com>
2019-03-29 14:57:14 +0000

    first moisture test

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+#GPIO SETUP
+channel = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("No water detected")
+    else:
+        print("Water detected")
+
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #event when pin goes high or low
+GPIO.add_event_callback(channel, callback)
+
+#loop
+while True:
+    time.sleep(1)
