873401582581964c64b2be30ab2fbb82e7f68475
JamieDF <jamiedf8@gmail.com>
2019-03-29 16:39:54 +0000

    updated test

--- test.py
+++ test.py
@@ -16,9 +16,14 @@ def callback(channel):
 
 
 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #event when pin goes high or low
-GPIO.add_event_callback(channel, callback)
+# GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #event when pin goes high or low
+# GPIO.add_event_callback(channel, callback)
 
 #loop
 while True:
+
+    if GPIO.input(channel):
+        print("Im wet baby")
+    else:
+        print("No water detected")
     time.sleep(1)
