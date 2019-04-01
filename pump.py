6b95a144a7cfa04c286b16fc0efd9978cab4df00
JamieDF <jamiedf8@gmail.com>
2019-04-01 10:54:53 +0100

    pump v1

--- pump.py
+++ pump.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
 
 def activate():
     print("Pump working")
+
+
+init = False
+
+GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
+
+def init_output(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.HIGH) 
+    
+#pump pin = 22
+def pump_on(pump_pin, delay):
+    init_output(pump_pin)
+    GPIO.output(pump_pin, GPIO.LOW)
+    time.sleep(delay)
+    GPIO.output(pump_pin, GPIO.HIGH)
