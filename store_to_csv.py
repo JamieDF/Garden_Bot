7787a7ac0e66f6826ee621f86f8bbb888f39cda3
Jamie <jamiedf8@gmail.com>
2019-08-23 18:19:01 +0100

    update

--- store_to_csv.py
+++ store_to_csv.py
@@ -30,3 +30,41 @@ def log(_LogEntry):
             w = csv.DictWriter(f, feildnames)
             w.writeheader()
             w.writerow(_LogEntry)
+
+def writeCSV(filename, data):
+
+    try:
+        fd = os.open(filename, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+    except OSError as e:
+        if e.errno == 17:
+            with open(filename, 'a') as f:
+                if isinstance(data, list):
+                    
+                    w = csv.DictWriter(f, list(data[0].keys()))
+                    for i in data:
+                        
+                        w.writerow(i)
+                else:
+                    
+                    w = csv.DictWriter(f, list(data.keys()))
+                    w.writerow(data)
+
+            return filename
+        else:
+            raise
+    else:
+        with open(filename, 'w') as f:
+            if isinstance(data, list):
+                
+                w = csv.DictWriter(f, list(data[0].keys()))
+                w.writeheader()
+                for i in data:
+                    
+                    w.writerow(i)
+            else:
+                
+                w = csv.DictWriter(f, list(data.keys()))
+                w.writeheader()
+                w.writerow(data)
+              
+    return filename
