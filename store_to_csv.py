d70610fcace0238795a0d5dfa99195a6fc233674
Jamie <jamiedf8@gmail.com>
2019-08-23 18:27:05 +0100

    fixed uploaded

--- store_to_csv.py
+++ store_to_csv.py
@@ -38,33 +38,14 @@ def writeCSV(filename, data):
     except OSError as e:
         if e.errno == 17:
             with open(filename, 'a') as f:
-                if isinstance(data, list):
-                    
-                    w = csv.DictWriter(f, list(data[0].keys()))
-                    for i in data:
-                        
-                        w.writerow(i)
-                else:
-                    
                     w = csv.DictWriter(f, list(data.keys()))
                     w.writerow(data)
 
             return filename
-        else:
-            raise
-    else:
-        with open(filename, 'w') as f:
-            if isinstance(data, list):
-                
-                w = csv.DictWriter(f, list(data[0].keys()))
-                w.writeheader()
-                for i in data:
-                    
-                    w.writerow(i)
-            else:
-                
-                w = csv.DictWriter(f, list(data.keys()))
-                w.writeheader()
-                w.writerow(data)
-              
+    
+    with open(filename, 'w') as f:
+        w = csv.DictWriter(f, list(data.keys()))
+        w.writeheader()
+        w.writerow(data)
+            
     return filename
